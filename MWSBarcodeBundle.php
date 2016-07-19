5ece1e09bff9036cab97f6e031d8562c80c9b470
Max Shtefec <max.shtefec@gmail.com>
2016-07-19 10:58:40 -0300

    nombres

---
+++ MWSBarcodeBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace MWSimple\BarcodeBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class MWSBarcodeBundle extends Bundle
+{
+}
--- SGKBarcodeBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace SGK\BarcodeBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class SGKBarcodeBundle extends Bundle
-{
-}
